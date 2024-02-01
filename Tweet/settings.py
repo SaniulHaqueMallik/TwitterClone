b1619d2cc2e34d08d6f38849cfb69a3f6df73f77
Saniul <saniulmallik@techis.io>
2024-02-01 22:43:10 +0530

    added requirements.txt

--- Tweet/settings.py
+++ Tweet/settings.py
@@ -25,9 +25,10 @@ SECRET_KEY = 'django-insecure-jiqprs&!_s#9=9ci#tqu-xf&-ctm7e75#1&($8%q++71v$ur+f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['*']
+X_FRAME_OPTIONS = '*'
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000', 'https://twitter-test-voe1.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
